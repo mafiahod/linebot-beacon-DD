241967eeaef994613b541a56c4670d6314e2c489
mafiahod <44860990+mafiahod@users.noreply.github.com>
2019-06-25 16:56:09 +0700

    Update user.js

--- src/core/model/user.js
+++ src/core/model/user.js
@@ -1,13 +1,6 @@
-const profile = require();
-function user(lineId, displayName) {
+module.exports=class userInfo{
+constructor(lineId, displayName) {
         this.lineId = lineId;
         this.displayName = displayName;
    
 }
-
-function getuserInfo(lineId, displayName) {
-    var userInfo = new user(lineId, displayName);
-    return userInfo;
-}
-
-//getuserInfo(profile.lineId,profile.displayName);
