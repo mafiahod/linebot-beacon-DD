ab246c99e95ed0a65e4b426c6239d3aaf76b6d5a
mafiahod <44860990+mafiahod@users.noreply.github.com>
2019-06-25 16:55:26 +0700

    Update activity.js

--- src/core/model/activity.js
+++ src/core/model/activity.js
@@ -1,12 +1,10 @@
-function activity(id, user, coming_in, coming_out, location, timestamp) {
+module.exports = class activityInfo{
+    constructor(id, user, coming_in, coming_out, location, timestamp) {
     this.id = id;
     this.user = user;
     this.coming_in = coming_in;
     this.coming_out = coming_out;
     this.location = location
     this.timestamp = timestamp;
-}
-function getactivityInfo(lineId, displayName) {
-    var activityInfo = new activity(lineId, displayName);
-    return activityInfo;
+
 }
