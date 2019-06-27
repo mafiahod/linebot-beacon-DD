433f8f567e7296ae56baea69bd7a9dcc49bc31b6
mafiahod <mafiahod@hotmail.com>
2019-06-27 11:14:22 +0700

    Update condition.js

--- src/core/model/condition.js
+++ src/core/model/condition.js
@@ -1,6 +1,16 @@
 module.exports = class Condition{
-    constructor()
+    constructor(objtype,value_where,number,value_by,type)
     {
+        this.select = objtype;
+        this.where = {
+            'userid': value1
+        };
+        this.count = number;
+        this.order = {
+            'by' : value2 ,
+            'desc' : type,
+           
+        };
         
     }
 }
