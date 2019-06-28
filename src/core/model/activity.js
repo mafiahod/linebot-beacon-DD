c32ebc0f259e4853f053d0301368fb65471e07be
mafiahod <mafiahod@hotmail.com>
2019-06-28 12:25:07 +0700

    Update activity.js

--- src/core/model/activity.js
+++ src/core/model/activity.js
@@ -1,11 +1,10 @@
 module.exports =  class Activity{
-    constructor(id,user,coming,timestamp,location,askstate,activityInfo) {
+    constructor(id,user,coming,timestamp,location,activityInfo) {
     this.userId = id;
     this.name = user;
     this.type = coming;
     this.timestamp = timestamp;
     this.location = location;
-    this.askstate = askstate; 
     this.plan = activityInfo;   
     }
 }
