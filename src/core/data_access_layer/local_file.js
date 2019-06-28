ccf1cd83690af7126d36954eb53f5a21a6880a29
ballkittipat272 <38604484+ballkittipat272@users.noreply.github.com>
2019-06-28 22:27:36 +0700

    Update local_file.js

--- src/core/data_access_layer/local_file.js
+++ src/core/data_access_layer/local_file.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const ActivityModel = require('../model/activity');
 const UserModel = require('../model/user');
-const ConditionModel = require('../model/');
 const current_datetime = new Date();
 const activityDir = './resource/' + current_datetime.getDate() + "-" +(current_datetime.getMonth() + 1) +  "-" + current_datetime.getFullYear()+'.json';
 const userDir = './resource/user.json';
@@ -10,11 +9,10 @@ const userDir = './resource/user.json';
 module.exports = {
     
     saveInform: function(obj){
-        var locationDir;
         if(obj instanceof ActivityModel){
-            locationDir = activityDir;
+            var locationDir = activityDir;
         }else if(obj instanceof UserModel){
-            locationDir = userDir;
+            var locationDir = userDir;
         }else{
             console.log("Unknow location to save");
         }
@@ -49,114 +47,45 @@ module.exports = {
     },
 
     
-    findInform: function(obj){
-        var locationDir = eval(obj.select+'Dir');
-        if(fs.existsSync(locationDir)){
+    findInform: function(obj,count,desc){
+        if(obj instanceof ActivityModel){
+            var locationDir = activityDir;
+        }else if(obj instanceof UserModel){
+            var locationDir = userDir;
+        }else{
+            console.log("Unknow location to find");
+        }
+        if(fs.existsSync(locationDir)) {
             var data = fs.readFileSync(locationDir);
             var dataArray = JSON.parse(data);
             var resultArray = [];
-            var returnArray = [];
-            var count = obj.count;
-            if(obj.count == null){
+            var propCount = 0;
+            var equalProp =0;
+            if(count == null){
                 count = dataArray.length;
             }
-            for(i=0 ; i<dataArray.length ; i++){
-                if(obj.where.userId !== null){
-                    if(dataArray[i].userId == obj.where.userId){
-                        resultArray.push(dataArray[i]);
+            for(i in dataArray){
+                for(property in obj){
+                    if(obj[property] == dataArray[i][property]){
+                        equalProp++;
                     }
-                }else if(obj.where.userId !== null && obj.where.location !== null){
-                    if(dataArray[i].userId == obj.where.userid && dataArray[i].location == obj.where.location){
-                        resultArray.push(dataArray[i]);
-                    }
-                }else if(obj.where.userId !== null && obj.where.location !== null && obj.where.plan !== null){
-                    if(dataArray[i].userId == obj.where.userid && dataArray[i].location == obj.where.location 
-                    && dataArray[i].plan != null){
-                        resultArray.push(dataArray[i]);
+                    if(obj[property] != null){
+                        propCount++;
                     }
                 }
+                if(equalProp == propCount){
+                    resultArray.push(dataArray[i]);
+                    propCount = 0;
+                    equalProp = 0;
+                }
             }
-            if(obj.order.desc == true){
+            if(desc == true){
                 resultArray.reverse();
             }
-            for(i = 0 ; i < count ; i++){
-                returnArray.push(resultArray[i]);
-            }
-            return returnArray;
-
-            /*var data = fs.readFileSync(locationDir);
-            var dataArray = JSON.parse(data);
-            var resultArray = [];
-
-            for(i=0 ; i<dataArray.length ; i++){
-                for (var property in obj) {
-                    if (obj.hasOwnProperty(property)) {
-                        //console.log(typeof obj[property]);
-                      
-                        if(typeof obj[property] == "object"){
-                            //console.log(obj[property]);
-                            
-                            for(var property1 in obj[property]){
-                                if(obj[property].hasOwnProperty(property1)){
-                                    console.log(obj[property][property1]);
-                                }
-                                
-                            }
-                        }
-
-
-                        if(dataArray[i][property] == obj[property]){
-                        
-
-
-
-                        }
-        
-                    }
-            
-                }
-            }*/
+            return resultArray.slice(0,count);
         }else{
-            console.log("There is no file");
+            console.log("There is no file to find");
         }
 
-
-
-
-        /*if(fs.existsSync(locationDir)){
-            var data = fs.readFileSync(locationDir);
-            var dataArray = JSON.parse(data);
-            var resultArray = [];
-            var loopCount = 0;
-            var wantCount = obj.count;
-            if(obj.count == null){
-                wantCount = dataArray.length;
-            }
-            if(obj.order.desc == false){
-                for(i=0 ; i<dataArray.length ; i++){
-                    if(dataArray[i].userId == obj.where.userid){
-                        resultArray.push(dataArray[i]);
-                        loopCount++;
-                    }
-                    if(loopCount >= wantCount){
-                        return resultArray;
-                    }
-                }
-            }
-            else if(obj.order.desc == true){
-                for(i=dataArray.length ; i>0 ; i--){
-                    if(dataArray[i-1].userId == obj.where.userid){
-                        resultArray.push(dataArray[i-1]);
-                        loopCount++;
-                    }
-                    if(loopCount >= wantCount){
-                        return resultArray;
-                    }
-                }
-                return resultArray;
-            }
-        }else{
-            console.log("There is no file");
-        }*/
     }
 }
