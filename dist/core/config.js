e8d0bf8fc3faabb119985c69ec7d7aff5ce30b83
jaja_pp <59011178@kmitl.ac.th>
2019-07-03 17:34:16 +0700

    remove nodemodule dist

--- dist/core/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-
-    port: "3000",
-    channelAccessToken: process.env.channelAccessToken,
-    channelSecret: process.env.channelSecret,
-    ReportGroupId: "C6ce76af8d6b199b60bb675b85e8cea2f"
-};
