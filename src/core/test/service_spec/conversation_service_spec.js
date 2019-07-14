778b1c222dfae7bbffd15a59167392c60847da62
ballkittipat272 <38604484+ballkittipat272@users.noreply.github.com>
2019-07-14 16:11:29 +0700

    Update conversation_service_spec.js

--- src/core/test/service_spec/conversation_service_spec.js
+++ src/core/test/service_spec/conversation_service_spec.js
@@ -5,7 +5,7 @@ var push = new Conversation_service();
 describe('callback', () => {
 
 
-    it('should push message even late reply ',(done) => {
+    it('should push 3 message even late reply ',(done) => {
 
         var check_ans = [];
         var activity = {
@@ -20,26 +20,74 @@ describe('callback', () => {
         check_ans.push(activity);
        
         var resultArray = [];
-       
-
+    
         push.message_service.send_Message = function mock_sendMessage(id, message) {
-           
-            
             resultArray.push({
                 toId: id,
                 message: message
             });
-
         };
 
 
         let count = 0;
 
         push.callback = function mock_callback(check_ans) {
+            setTimeout(() => {
+                if (check_ans[0].plan == 'none' && count < 3) {
+
+                    push.message_service.send_Message("1234", "hello");
+
+                    count++;
+
+                    push.callback(check_ans);
+
+                } else if (check_ans[0].plan != 'none') {
+                    console.log("exist loop from conver,callback");
+                    return;
+                }
 
 
-          
+            }, 1000)
+
+        };
        
+        push.callback(check_ans);
+      
+        setTimeout(() => {
+            console.log(resultArray);
+            expect(resultArray).toEqual([ { toId : '1234', message : 'hello' }, { toId : '1234', message : 'hello' }, { toId : '1234', message : 'hello' } ]);
+            done();
+         
+            }, 4000);
+    });
+
+    it('should push only 2 message when activity.plan has updated at second callback()',(done) => {
+
+        var check_ans = [];
+        var activity = {
+            "userId": "U9f12e85f8a0d10571a4af43eacd9e127",
+            "name": "..BALL..",
+            "type": "in",
+            "timestamp": 1562774750526,
+            "location": "Dimension Data Office, Asok",
+            "askstate": true,
+            "plan": "none"
+        };
+        check_ans.push(activity);
+       
+        var resultArray = [];
+    
+        push.message_service.send_Message = function mock_sendMessage(id, message) {
+            resultArray.push({
+                toId: id,
+                message: message
+            });
+        };
+
+
+        let count = 0;
+
+        push.callback = function mock_callback(check_ans) {
             setTimeout(() => {
                 
                 if(count == 2 ){
@@ -65,8 +113,6 @@ describe('callback', () => {
 
         };
        
-        
-      
         push.callback(check_ans);
       
         setTimeout(() => {
@@ -75,9 +121,5 @@ describe('callback', () => {
             done();
          
             }, 4000);
-       
-         
-         
-
     });
 });
