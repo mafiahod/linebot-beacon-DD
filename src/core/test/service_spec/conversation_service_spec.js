e8c28269fd55303f545a2873f794d0ff866be33b
jaja_pp <59011178@kmitl.ac.th>
2019-07-14 15:48:51 +0700

    update callback_spec

--- src/core/test/service_spec/conversation_service_spec.js
+++ src/core/test/service_spec/conversation_service_spec.js
@@ -1,12 +1,11 @@
 'use strict';
 import { Conversation_service } from '../../service/index'
-
 var push = new Conversation_service();
 
 describe('callback', () => {
 
 
-    it('should push message even late reply ', () => {
+    it('should push message even late reply ',(done) => {
 
         var check_ans = [];
         var activity = {
@@ -19,11 +18,13 @@ describe('callback', () => {
             "plan": "none"
         };
         check_ans.push(activity);
-
+       
         var resultArray = [];
+       
 
         push.message_service.send_Message = function mock_sendMessage(id, message) {
-
+           
+            
             resultArray.push({
                 toId: id,
                 message: message
@@ -37,27 +38,37 @@ describe('callback', () => {
         push.callback = function mock_callback(check_ans) {
 
 
+          
+       
             setTimeout(() => {
                 if (check_ans[0].plan == 'none' && count < 3) {
 
                     push.message_service.send_Message("1234", "hello");
-                    
+
                     count++;
-                    
+
                     push.callback(check_ans);
-                    
+
                 }
-                
+
 
             }, 1000)
 
         };
-        push.callback(check_ans);
-        expect(resultArray).toEqual([
-        { toId: '1234', message: 'hello' },
-        { toId: '1234', message: 'hello' },
-        { toId: '1234', message: 'hello' } ]);
+       
         
       
+        push.callback(check_ans);
+      
+        setTimeout(() => {
+          
+            expect(resultArray).toEqual([ { toId : '1234', message : 'hello' }, { toId : '1234', message : 'hello' }, { toId : '1234', message : 'hello' } ]);
+            done();
+         
+            }, 4000);
+       
+         
+         
+
     });
 });
