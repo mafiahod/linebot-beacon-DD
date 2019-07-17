6dff7f496a017f48c603e2519306c892f66e675d
jaja_pp <59011178@kmitl.ac.th>
2019-07-17 10:36:17 +0700

    Update conversation_service_spec.js

--- src/core/test/service_spec/conversation_service_spec.js
+++ src/core/test/service_spec/conversation_service_spec.js
@@ -24,13 +24,13 @@ describe('callback', () => {
             });
         };
 
-        let count = 0;
-        push.callback = function mock_callback(check_ans) {
+        push.callback = function mock_callback(check_ans,count) {
             setTimeout(() => {
                 if (check_ans[0].plan == 'none' && count < 3) {
-                    push.message_service.send_Message("1234", "hello");
+                    push.message_service.send_Message("U9f12e85f8a0d10571a4af43eacd9e127", "hello");
                     count++;
-                    push.callback(check_ans);
+                   
+                    push.callback(check_ans,count);
                 } else if (check_ans[0].plan != 'none') {
                     console.log("exist loop from conver,callback");
                     return;
@@ -38,9 +38,9 @@ describe('callback', () => {
             }, 1000)
         };
 
-        push.callback(check_ans);
+        push.callback(check_ans,0);
         setTimeout(() => {
-            expect(resultArray).toEqual([{ toId: '1234', message: 'hello' }, { toId: '1234', message: 'hello' }, { toId: '1234', message: 'hello' }]);
+            expect(resultArray).toEqual([{ toId: 'U9f12e85f8a0d10571a4af43eacd9e127', message: 'hello' }, { toId: 'U9f12e85f8a0d10571a4af43eacd9e127', message: 'hello' }, { toId: 'U9f12e85f8a0d10571a4af43eacd9e127', message: 'hello' }]);
             done();
 
         }, 4000);
@@ -69,16 +69,16 @@ describe('callback', () => {
             });
         };
 
-        let count = 0;
-        push.callback = function mock_callback(check_ans) {
+      
+        push.callback = function mock_callback(check_ans,count) {
             setTimeout(() => {
                 if (count == 2) {
                     activity.plan = "work";
                 }
                 if (check_ans[0].plan == 'none' && count < 3) {
-                    push.message_service.send_Message("1234", "hello");
+                    push.message_service.send_Message("U9f12e85f8a0d10571a4af43eacd9e127", "hello");
                     count++;
-                    push.callback(check_ans);
+                    push.callback(check_ans,count);
                 } else if (check_ans[0].plan != 'none') {
                     console.log("exist loop from conver,callback");
                     return;
@@ -86,10 +86,55 @@ describe('callback', () => {
             }, 1000)
         };
 
-        push.callback(check_ans);
+        push.callback(check_ans,0);
         setTimeout(() => {
-            expect(resultArray).toEqual([{ toId: '1234', message: 'hello' }, { toId: '1234', message: 'hello' }]);
+            expect(resultArray).toEqual([{ toId: 'U9f12e85f8a0d10571a4af43eacd9e127', message: 'hello' }, { toId: 'U9f12e85f8a0d10571a4af43eacd9e127', message: 'hello' }]);
             done();
         }, 4000);
     });
+
+    it('should push message 6 times if users are arrive 2 people', (done) => {
+        var check_ans = [];
+        var activity = {
+            "userId": "U9f12e85f8a0d10571a4af43eacd9e127",
+            "name": "..BALL..",
+            "type": "in",
+            "timestamp": 1562774750526,
+            "location": "Dimension Data Office, Asok",
+            "askstate": true,
+            "plan": "none"
+        };
+
+        check_ans.push(activity);
+        var resultArray = [];
+        push.message_service.send_Message = function mock_sendMessage(id, message) {
+            resultArray.push({
+                toId: id,
+                message: message
+            });
+        };
+
+      
+        push.callback = function mock_callback(check_ans,count) {
+            setTimeout(() => {
+                
+                if (check_ans[0].plan == 'none' && count < 3) {
+                    push.message_service.send_Message("U9f12e85f8a0d10571a4af43eacd9e127", "hello");
+                    count++;
+                    push.callback(check_ans,count);
+                } else if (check_ans[0].plan != 'none') {
+                    console.log("exist loop from conver,callback");
+                    return;
+                }
+            }, 1000)
+        };
+
+        push.callback(check_ans,0);
+        push.callback(check_ans,0);
+        setTimeout(() => {
+            expect(resultArray.length).toEqual(6);
+            done();
+        }, 4000);
+    });
+
 });
