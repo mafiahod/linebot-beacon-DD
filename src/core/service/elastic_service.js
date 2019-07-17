1d4a6702f021c402c4b61fb652bfec7132171996
mafiahod <mafiahod@hotmail.com>
2019-07-17 21:54:08 +0700

    ll

--- src/core/service/elastic_service.js
+++ src/core/service/elastic_service.js
@@ -1,42 +1,46 @@
 import * as elasticsearch from 'elasticsearch'
-import { doesNotThrow } from 'assert';
 import { logger } from '../../../logs/logger';
-
+const current_datetime = new Date();
 var client = new elasticsearch.Client({
     host: 'localhost:9200',
-    // log: 'trace'
+     log: 'trace'
 });
 // var thescript ={
 //     "inline" :`ctx._source.plan='${obj.plan}';`
 // }
 
-
-async function elastic_update(obj) {
-    await client.updateByQuery({
-        index: 'myindex',
+async function elastic_update(obj,property) {
+    console.log("-------------------------------------")
+    console.log(obj[property]);
+   // console.log(obj);
+   //let a = obj[property];
+   await  client.updateByQuery({
+        index: 'myindex',//+current_datetime.getDate() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getFullYear(),
         type: '_doc',
-        body: {
-            "query": { "match": { "userId": obj.userId } },
-            "script": { "inline": `ctx._source.askstate='${obj.askstate}';  ` }
-        }
-    }, (err) => {
-        logger.error(err);
-    })
+        body: {          
+            "query": { "match": { "obj.userId": obj.userId} },            
+            "script": { "inline": `ctx._source.obj.${property}='${obj[property]}';` }
+        }      
+    }, (result) => {
+        logger.info("elasticupdate");
+        logger.error(result);
+    });
+
 }
 
 
 async function elastic_save(obj) {
     await client.index({
-        index: 'myindex',
+        index: 'myindex',//+current_datetime.getDate() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getFullYear(),
         type: '_doc',
         body: {
             obj
         }
-    }, (err) => {
+    }, (result) => {
+        logger.info("elasticsave");
+        logger.error(result);
 
-        logger.error(err);
-
-    })
+    });
 }
 
 class Elastic_service {
